0348bd0febc11224dcc85df2ba121f629a060ecd
tokuchan <kazuki.ok.715@gmail.com>
2018-10-29 22:50:36 +0900

    コーナー検出追加

--- example.py
+++ example.py
@@ -1,77 +1,70 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
 
 import cv2
 import numpy as np
 
 def main():
-    #画像の読み込み
-    img = cv2.imread("./input/example.png",1)
-
-    #グレースケール変換
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # showImage(img)
+    #カラー画像の読み取り
+    img_src = cv2.imread("./input/testimg1.png",1)
+    
+    #グレースケール
+    img_gray = cv2.cvtColor(img_src,cv2.COLOR_BGR2GRAY)
 
     #二値化
-    thresh = 172    #ここの値がピースによって変わる可能性がある
-    max_pixel = 255
-    _, img_Binary = cv2.threshold(img_gray,
-                                 thresh,
-                                 max_pixel,
-                                 cv2.THRESH_BINARY)
+    thresh = 172
+    max_value = 255
+    _, img_binary = cv2.threshold(img_gray,
+                                  thresh,
+                                  max_value,
+                                  cv2.THRESH_BINARY_INV)
 
-    #膨張と縮小とラベリングでごみ取り
-    #kernel = np.ones((15,15),np.uint8)
-    #erode = cv2.erode(img_Binary,kernel) #白の領域が収縮
-    #img_Binary = cv2.dilate(erode,kernel)
-    
+    #ノイズ除去
+    kernel = np.ones((20,20),np.uint8)
+    dilate = cv2.dilate(img_binary,kernel) #白の領域が膨張
+    img_binary = cv2.erode(dilate,kernel) #白の領域が収縮
+   
     #ラベリング処理
-    img_Binary = cv2.bitwise_not(img_Binary)
-    Labels = cv2.connectedComponentsWithStats(img_Binary)
-    #オブジェクト情報を項目別に抽出
+    #img_binary = cv2.bitwise_not(img_binary)
+    Labels = cv2.connectedComponentsWithStats(img_binary)
+    #オブジェクト情報を抽出
     LabelNum = Labels[0] - 1
     data = np.delete(Labels[2],0,0)
     center = np.delete(Labels[3],0,0)
-    
-    color_src1 = cv2.cvtColor(img_Binary,cv2.COLOR_GRAY2BGR)
+
+    img_color = cv2.cvtColor(img_binary,cv2.COLOR_GRAY2BGR)
 
     for i in range(LabelNum):
-        #各オブジェクトの概説矩形を表示
         x0 = data[i][0]
         y0 = data[i][1]
         x1 = data[i][0] + data[i][2]
         y1 = data[i][1] + data[i][3]
-        cv2.rectangle(color_src1,(x0,y0),(x1,y1),(0,0,255))
-        cv2.putText(color_src1,
-                    "ID:"+str(i+1),
+        cv2.rectangle(img_color,(x0,y0),(x1,y1),(0,0,255))
+        cv2.putText(img_color,
+                    "ID:"+str(i),
                     (x1-20,y1+15),
                     cv2.FONT_HERSHEY_PLAIN,
-                    1,(0,255,255)) 
-        cv2.putText(color_src1,
-                     str(int(center[i][0]))+" "+str(int(center[i][1])), 
-                    (int(center[i][0]),int(center[i][1])), 
-                    cv2.FONT_HERSHEY_PLAIN, 
-                    1, (0, 255,0))
-    
-    showImage(color_src1)
-#    dilate = cv2.dilate(erode,kernel) #白の領域が膨張
+                    1,(0,255,255))
 
-    #エッジ検出処理
-    canny_img = cv2.Canny(img_Binary,50,200)
+    showImage(img_color)
 
-    #辺の特徴検出
-    akaze = cv2.AKAZE_create()
-    kp1 = akaze.detect(img_Binary)
-    img_akaze = cv2.drawKeypoints(img_Binary,kp1,None,flags=4)
-    cv2.imwrite("./output/result.png",img_akaze)
-    cv2.imwrite("./output/labeling.png",color_src1)
-    #DB連携
+    #コーナー検出
+    img_corner = cv2.cornerHarris(img_binary,2,3,0.04) 
+    img_corner = cv2.dilate(img_corner,None,iterations=5)
+    img_binary = cv2.cvtColor(img_binary,cv2.COLOR_GRAY2BGR)
+    img_harris = img_binary.copy()
+    img_harris = cv2.bitwise_not(img_harris)
+    img_harris[img_corner>0.04*img_corner.max()]=[0,0,255]
+    showImage(img_harris)
 
-#画像の表示関数
+    cv2.imwrite("./output/testimg1_labeling.png",img_color)
+    cv2.imwrite("./output/testimg1_harris.png",img_harris)
+
+# 画像の表示関数
 def showImage(img):
-    cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("img",cv2.WINDOW_NORMAL)
     cv2.imshow("img",img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 if __name__=='__main__':
     main()
+
