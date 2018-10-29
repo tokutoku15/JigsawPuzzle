fcb640fe37690d4766b37f3be8bc6c46d5540351
tokuchan <kazuki.ok.715@gmail.com>
2018-10-29 17:51:06 +0900

    exampleの画像でお試し中

--- example.py
+++ example.py
@@ -5,44 +5,65 @@ import numpy as np
 
 def main():
     #画像の読み込み
-    img = cv2.imread("./input/testimg1.png",1)
+    img = cv2.imread("./input/example.png",1)
 
     #グレースケール変換
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # showImage(img)
 
     #二値化
-    thresh = 180    #ここの値がピースによって変わる可能性がある
+    thresh = 172    #ここの値がピースによって変わる可能性がある
     max_pixel = 255
-    ret, img_dst = cv2.threshold(img_gray,
+    _, img_Binary = cv2.threshold(img_gray,
                                  thresh,
                                  max_pixel,
                                  cv2.THRESH_BINARY)
 
-    #膨張と縮小でごみ取り
-    kernel = np.ones((20,20),np.uint8)
-    erode = cv2.erode(img_dst,kernel) #白の領域が収縮
-    dilate = cv2.dilate(erode,kernel) #白の領域が膨張
-    dilate2 = cv2.dilate(dilate,kernel)
-    dst = cv2.erode(dilate2,kernel)
-
+    #膨張と縮小とラベリングでごみ取り
+    #kernel = np.ones((15,15),np.uint8)
+    #erode = cv2.erode(img_Binary,kernel) #白の領域が収縮
+    #img_Binary = cv2.dilate(erode,kernel)
     
+    #ラベリング処理
+    img_Binary = cv2.bitwise_not(img_Binary)
+    Labels = cv2.connectedComponentsWithStats(img_Binary)
+    #オブジェクト情報を項目別に抽出
+    LabelNum = Labels[0] - 1
+    data = np.delete(Labels[2],0,0)
+    center = np.delete(Labels[3],0,0)
+    
+    color_src1 = cv2.cvtColor(img_Binary,cv2.COLOR_GRAY2BGR)
+
+    for i in range(LabelNum):
+        #各オブジェクトの概説矩形を表示
+        x0 = data[i][0]
+        y0 = data[i][1]
+        x1 = data[i][0] + data[i][2]
+        y1 = data[i][1] + data[i][3]
+        cv2.rectangle(color_src1,(x0,y0),(x1,y1),(0,0,255))
+        cv2.putText(color_src1,
+                    "ID:"+str(i+1),
+                    (x1-20,y1+15),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    1,(0,255,255)) 
+        cv2.putText(color_src1,
+                     str(int(center[i][0]))+" "+str(int(center[i][1])), 
+                    (int(center[i][0]),int(center[i][1])), 
+                    cv2.FONT_HERSHEY_PLAIN, 
+                    1, (0, 255,0))
+    
+    showImage(color_src1)
+#    dilate = cv2.dilate(erode,kernel) #白の領域が膨張
+
     #エッジ検出処理
-    canny_img = cv2.Canny(dst,50,200)
-    
-    # showImage(dst)
-    # showImage(canny_img)
-
-    # akaze = cv2.AKAZE_create()
-    # kp1 = akaze.detect(img_dst)
-    # img_akaze = cv2.drawKeypoints(img_dst,kp1,None,flags=4)
-
-    cv2.imwrite("./output/example_result1.png",dst)
-    cv2.imwrite("./output/example_result2.png",canny_img)
-    # cv2.imwrite("./output/example_result3.png",img_akaze)
+    canny_img = cv2.Canny(img_Binary,50,200)
 
     #辺の特徴検出
-
+    akaze = cv2.AKAZE_create()
+    kp1 = akaze.detect(img_Binary)
+    img_akaze = cv2.drawKeypoints(img_Binary,kp1,None,flags=4)
+    cv2.imwrite("./output/result.png",img_akaze)
+    cv2.imwrite("./output/labeling.png",color_src1)
     #DB連携
 
 #画像の表示関数
